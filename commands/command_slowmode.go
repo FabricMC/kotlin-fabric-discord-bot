a4ef2676791a9e989a2f9567ca7e577d81426e81
AppleTheGolden <scotsbox@protonmail.com>
2020-08-20 17:12:49 +0200

    Fix slowmode hoisting channels to the top

--- commands/command_slowmode.go
+++ commands/command_slowmode.go
@@ -40,53 +40,57 @@ func SlowmodeCommand(ctx *discord.CommandContext) error {
 	} else {
 		channelID = ctx.Message.ChannelID
 	}
+	/*
+		(Apple:)
+		Blegh. Discordgo's structs, when marshalled with json.Marshal(), don't
+		include empty values, due to the `json:"omitempty"` directive on all their fields.
+		This makes sense, because this is a PATCH endpoint, but the issue is that
+		RateLimitPerUser is an int.
+		Ints can't be nil, but they still have a defined empty value: 0.
+		This is very inconvenient for us because the json marshaller sees the 0 and
+		the `omitempty` directive, assumes the 0 is a default value, and doesn't include it.
+		Discord then interprets not sending this field as not wanting to change it.
 
-	var err error
-	if time > 0 {
-		_, err = ctx.Session.ChannelEditComplex(channelID, &discordgo.ChannelEdit{RateLimitPerUser: int(time)})
+		To work around this, we send the request to the channel endpoint ourselves
+		with a custom struct that doesn't omit 0 values and all is well in the world.
 
-		if err == nil {
-			err = ctx.SendMessageWithAudit("Slow mode enabled", "Enabled slow mode ("+strconv.FormatUint(time, 10)+"s) in <#"+channelID+">")
-		}
-	} else {
-		/*
-			(Apple:)
-			Blegh. Discordgo's structs, when marshalled with json.Marshal(), don't
-			include empty values, due to the `json:"omitempty"` directive on all their fields.
-			This makes sense, because this is a PATCH endpoint, but the issue is that
-			RateLimitPerUser is an int.
-			Ints can't be nil, but they still have a defined empty value: 0.
-			This is very inconvenient for us because the json marshaller sees the 0 and
-			the `omitempty` directive, assumes the 0 is a default value, and doesn't include it.
-			Discord then interprets not sending this field as not wanting to change it.
+		Except that the upstream fix for this - changing the int field to *int which can
+		discern between nil and 0 - is a breaking change.
+		Also, this problem spans across the entirety of discordgo.
 
-			To work around this, we send the request to the channel endpoint ourselves
-			with a custom struct that doesn't omit 0 values and all is well in the world.
+		Yay.
 
-			Except that the upstream fix for this - changing the int field to *int which can
-			discern between nil and 0 - is a breaking change.
-			Also, this problem spans across the entirety of discordgo.
+		Update 2020-08-20:
+		Using discordgo's function to edit a channel hoists it to the top of its category.
+		I don't even understand this one, but switching both codepaths to use a manual request
+		works, so I'm not going to question it too much.
+	*/
 
-			Yay.
-		*/
-		var body []byte
-		body, err = ctx.Session.RequestWithBucketID("PATCH",
-			discordgo.EndpointChannel(channelID),
-			struct {
-				RateLimit uint64 `json:"rate_limit_per_user"`
-			}{time},
-			discordgo.EndpointChannel(channelID),
-		)
+	err := betterEditChannel(ctx, channelID, struct {
+		RateLimit uint64 `json:"rate_limit_per_user"`
+	}{time})
 
-		if err != nil {
-			return err
-		}
-		err = json.Unmarshal(body, &discordgo.Channel{})
+	if err == nil {
+		if time > 0 {
+			err = ctx.SendMessageWithAudit("Slowmode enabled", "Enabled slowmode ("+strconv.FormatUint(time, 10)+"s) in <#"+channelID+">")
 
-		if err == nil {
-			err = ctx.SendMessageWithAudit("Slow mode disabled", "Disabled slow mode in <#"+channelID+">")
+		} else {
+			err = ctx.SendMessageWithAudit("Slowmode disabled", "Disabled slowmode in <#"+channelID+">")
 		}
 	}
-
 	return err
+}
+
+func betterEditChannel(ctx *discord.CommandContext, channelID string, request interface{}) error {
+	body, err := ctx.Session.RequestWithBucketID("PATCH",
+		discordgo.EndpointChannel(channelID),
+		request,
+		discordgo.EndpointChannel(channelID),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, &discordgo.Channel{})
 }
